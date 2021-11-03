7211055b02852b8492197b739b585ed136524821
wookoo <nanayagoon@daum.net>
2021-11-03 14:12:32 +0900

    axios 예제 구현

--- src/App.js
+++ src/App.js
@@ -1,68 +1,66 @@
-
 //6t9h6nikto
 
-import { useState } from 'react';
-import { NaverMap, Marker } from 'react-naver-maps';
-import { RenderAfterNavermapsLoaded } from 'react-naver-maps'; // 패키지 불러오기
+import { useState } from "react";
+import { NaverMap, Marker } from "react-naver-maps";
+import { RenderAfterNavermapsLoaded } from "react-naver-maps"; // 패키지 불러오기
 
-
-function NaverMapAPI({lat,lon}){
-
-
-  
-
-  return(
-  
-
+const axios = require("axios");
+function NaverMapAPI({ lat, lon }) {
+  return (
     <NaverMap
-    center={{ lat: lat, lng: lon }}
-    style={{
-      width: '100%', // 네이버지도 가로 길이
-      height: '85vh' // 네이버지도 세로 길이
-    }}
-    
+      center={{ lat: lat, lng: lon }}
+      style={{
+        width: "100%", // 네이버지도 가로 길이
+        height: "70vh", // 네이버지도 세로 길이
+      }}
     >
       <Marker
-        key = {1}
+        key={1}
         position={{ lat: lat, lng: lon }}
         //onClick={() => {setLat(36.7672084337826); setLon(126.9381987088473);}}
       />
-
     </NaverMap>
-    
-  
-
   );
 }
 function App() {
-  const [lat,setLat] = useState(37.3595704);
-  const [lon,setLon] = useState(127.105399);
+  const [lat, setLat] = useState(37.3595704);
+  const [lon, setLon] = useState(127.105399);
   return (
     <div>
-    <RenderAfterNavermapsLoaded
-      ncpClientId={'6t9h6nikto'} // 자신의 네이버 계정에서 발급받은 Client ID
-      error={<p>Maps Load Error</p>}
-      loading={<p>Maps Loading...</p>}
-    >
-      <NaverMapAPI
-      lat={lat}
-      lon={lon}
-      />
+      <RenderAfterNavermapsLoaded
+        ncpClientId={"6t9h6nikto"} // 자신의 네이버 계정에서 발급받은 Client ID
+        error={<p>Maps Load Error</p>}
+        loading={<p>Maps Loading...</p>}
+      >
+        <NaverMapAPI lat={lat} lon={lon} />
+      </RenderAfterNavermapsLoaded>
 
-    </RenderAfterNavermapsLoaded>
-  
-
-    <div style={{
-      backgroundColor:'#0f0',
-      overflow:'scroll',
-      width:'100%' ,
-      height:'15vh'}}>
-          <button
-      onClick={() => {setLat(36.761707697467465); setLon(126.93377842852455);}}
-      >버튼</button>
-      <button
-      onClick={() => {setLat(36.77068057043212); setLon(126.92485203719319);}}
-      >버튼2</button>
+      <div
+        style={{
+          backgroundColor: "#0f0",
+          overflow: "scroll",
+          width: "100%",
+          height: "auto",
+        }}
+      >
+        <button
+          onClick={async () => {
+            setLat(36.761707697467465);
+            setLon(126.93377842852455);
+            var r = await axios.get("http://localhost:8000/bus/gps/download/");
+            console.log(r);
+          }}
+        >
+          버튼
+        </button>
+        <button
+          onClick={() => {
+            setLat(36.77068057043212);
+            setLon(126.92485203719319);
+          }}
+        >
+          버튼2
+        </button>
         <ul>
           <li>1</li>
           <li>1</li>
@@ -71,9 +69,9 @@ function App() {
           <li>1</li>
           <li>1</li>
         </ul>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
 export default App;
