1220f1fb3e3a08413b59015dbf7deb43d6c45a0c
wookoo <nanayagoon@daum.net>
2021-11-03 19:54:39 +0900

    class 변경 및 위치 자동갱신

--- src/App-bacup.js
+++ src/App-bacup.js
@@ -1,69 +1,10 @@
 //6t9h6nikto
+
+import { useState } from "react";
 import { NaverMap, Marker } from "react-naver-maps";
 import { RenderAfterNavermapsLoaded } from "react-naver-maps"; // 패키지 불러오기
 
-import axios from "axios";
-import React from "react";
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-class App extends React.Component {
-  state = {
-    lat: 37.3595704,
-    lon: 127.105399,
-  };
-  getPosition = async () => {
-    const {
-      data: { lat, lon },
-    } = await axios.get("http://localhost:8000/bus/gps/download/");
-    this.setState({ lat: lat, lon: lon });
-    console.log(lat, lon);
-  };
-  async componentDidMount() {
-    while (1) {
-      this.getPosition();
-      await sleep(1000);
-    }
-  }
-
-  render() {
-    const { lat, lon } = this.state;
-    return (
-      <div>
-        <RenderAfterNavermapsLoaded
-          ncpClientId={"6t9h6nikto"} // 자신의 네이버 계정에서 발급받은 Client ID
-          error={<p>Maps Load Error</p>}
-          loading={<p>Maps Loading...</p>}
-        >
-          <NaverMap
-            center={{ lat: lat, lng: lon }}
-            style={{
-              width: "100%", // 네이버지도 가로 길이
-              height: "70vh", // 네이버지도 세로 길이
-            }}
-          >
-            <Marker
-              key={1}
-              position={{ lat: lat, lng: lon }}
-              //onClick={() => {setLat(36.7672084337826); setLon(126.9381987088473);}}
-            />
-          </NaverMap>
-        </RenderAfterNavermapsLoaded>
-        <button
-          onClick={() => {
-            this.setState({ lat: 36.7672084337826, lon: 126.9381987088473 });
-          }}
-        >
-          버튼
-        </button>
-      </div>
-    );
-  }
-}
-
-/*
+const axios = require("axios");
 var onchange = false;
 
 function sleep(ms) {
@@ -153,5 +94,5 @@ function App() {
     </div>
   );
 }
-//get_server_data({ sr, sl });*/
+//get_server_data({ sr, sl });
 export default App;
