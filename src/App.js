67abd2dc11f4aa5f29ecded4babafeaea4cf3d32
wookoo <nanayagoon@daum.net>
2021-11-08 22:22:59 +0900

    REST API -> 웹소켓 변경 및 지도 마커 추가

--- src/App.js
+++ src/App.js
@@ -2,30 +2,73 @@
 import { NaverMap, Marker } from "react-naver-maps";
 import { RenderAfterNavermapsLoaded } from "react-naver-maps"; // 패키지 불러오기
 
-import axios from "axios";
 import React from "react";
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+
 
 class App extends React.Component {
   state = {
     lat: 37.3595704,
     lon: 127.105399,
+    tag: [],
+    bgColor : "red",
+    text: "운행종료",
+
   };
-  getPosition = async () => {
-    const {
-      data: { lat, lon },
-    } = await axios.get("http://192.168.1.71:8000/bus/gps/download/");
-    this.setState({ lat: lat, lon: lon });
-    console.log(lat, lon);
-  };
+
   async componentDidMount() {
-    while (1) {
-      this.getPosition();
-      await sleep(1000);
-    }
+  
+    let chatSocket = new WebSocket(
+      `ws://220.69.209.111:8000/ws/1234/`
+      );
+      chatSocket.onmessage = (e) => {
+        let data = JSON.parse(e.data);
+        
+        let lat = data['message']['lat']
+        let lon = data['message']['lon']
+        let tag = data['message']['tag']
+        let info = data['message']['info']
+        let user = data['user']
+        let connection = data['message']['connection']
+      
+          if(connection == "out" ){
+            //여기서 connect 나간거 작업 진행
+            this.setState({bgColor:"red",text:"운행종료"})
+            return;
+          }
+          else if(connection == "in"){
+            //여기서 connect 들어온거 진행
+            this.setState({bgColor:"green",text:"운행중"})
+            return;
+            
+          }
+          
+        
+
+        
+        if(lat != 0 && lon != 0){
+          this.setState({ lat: lat, lon: lon });
+
+        }
+        if(tag != null){
+          let temp = this.state.tag
+          let today = new Date();   
+
+          let hours = today.getHours(); // 시
+          let minutes = today.getMinutes();  // 분
+          let seconds = today.getSeconds();  // 초
+          let now = hours + ':' + minutes + ':' + seconds
+          let push_data = {tag:tag,lat:lat,lon:lon,info:info,time:now}
+          console.log(push_data);
+          
+          temp.push(push_data)
+          this.setState({tag:temp})
+          console.log(this.state.tag) 
+        }
+        
+    };
+  
+
   }
 
   render() {
@@ -45,22 +88,34 @@ class App extends React.Component {
             }}
           >
             <Marker
-              key={1}
+             
               position={{ lat: lat, lng: lon }}
               //onClick={() => {setLat(36.7672084337826); setLon(126.9381987088473);}}
             />
+
+            {this.state.tag.map( (x,index) => <Marker
+            key = {index}
+            position = {{lat:x.lat,lng : x.lon}}
+            onClick = { () => { alert(`${x.tag} ${x.info} - ${x.time}`) }}
+            />)}
           </NaverMap>
         </RenderAfterNavermapsLoaded>
-        <button
-          onClick={() => {
-            this.setState({ lat: 36.7672084337826, lon: 126.9381987088473 });
-          }}
+        <h2
+        style={
+          {
+            backgroundColor : this.state.bgColor,
+            width : "150px",
+            color : "#fff",
+          }
+        }
         >
-          버튼
-        </button>
+         {this.state.text}</h2>
+        <ul> {this.state.tag.reverse().map( (x) => <li>{x.tag}  {x.info}  - {x.time} </li>)}</ul>
       </div>
     );
   }
+
+  
 }
 
 /*
